d180498ea4e9c4d65393c779e3e45307d0f114f6
unknown <rufaidashafik@gmail.com>
2023-08-10 17:40:13 -0700

    Added SEO

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Header from './components/Header'
 import Section from './components/Section'
 import {DataProvider} from './components/context'
-
+import { Helmet } from 'react-helmet';
 class App extends React.Component{
   render(){
     return(
       <DataProvider>
       <div className="app">
+      <Helmet>
+  <title>Shopping Cart-React Application</title>
+</Helmet>
             <Router>
               <Header />
               <Section />
